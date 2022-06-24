9f71362251502ba5c0ef889af87d0ca83e69bdd7
Foyez39 <105016210+Foyez39@users.noreply.github.com>
2022-06-24 11:32:03 -0500

    Update PersonalInformationPage.java

--- src/main/java/pages/foyez/PersonalInformationPage.java
+++ src/main/java/pages/foyez/PersonalInformationPage.java
@@ -5,12 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 public class PersonalInformationPage extends CommonAPI {
 
-    private final Logger LOG = LoggerFactory.getLogger(PersonalInformationPage.class);
+ 
 
 @FindBy(xpath = "//span[text()='My personal information']")
 private WebElement personalInfo;
@@ -36,32 +35,32 @@ private WebElement submit;
 
     public void clickOnMyPersonalInfo(){
         click(personalInfo);
-       LOG.info("click on personal info success");
+     
 }
 
     public void selectDay(String option){
         selectFromDropdown(days,option);
-        LOG.info("select day  success");
+        
  }
 
     public void selectMonth(String option){
         selectFromDropdown(months,option);
-        LOG.info("select month success");
+  
     }
 
     public void selectYear(String option){
         selectFromDropdown(years,option);
-        LOG.info("select year success");
+        
     }
 
     public void enterCurrentPassword(String text){
         type(oldPassword,text);
-        LOG.info("enter current password success");
+       
     }
 
     public void clickOnSubmit(){
         click(submit);
-        LOG.info("click on submit success");
+   
     }
 
 }
