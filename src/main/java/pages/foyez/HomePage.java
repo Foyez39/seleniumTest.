bbbdf6c0f83f02e9641728ccf39baaf80d53aa48
Foyez39 <105016210+Foyez39@users.noreply.github.com>
2022-06-24 11:29:23 -0500

    Update HomePage.java

--- src/main/java/pages/foyez/HomePage.java
+++ src/main/java/pages/foyez/HomePage.java
@@ -5,12 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 public class HomePage extends CommonAPI {
 
-    private final Logger LOG = LoggerFactory.getLogger(HomePage.class);
+   
     @FindBy(css = "a.login")
     private WebElement SignIn;
 
@@ -51,45 +50,45 @@ public class HomePage extends CommonAPI {
 
     public void clickOnSignIn() {
         click(SignIn);
-        LOG.info("click on sign in success");
+     
     }
 
 
     public void searchElement(String item) {
         type(searchBox, item);
-        LOG.info("search element success");
+       
     }
 
     public void clickOnSearchBtn(){
         click(searchBtn);
-        LOG.info("click on search button success");
+      
     }
 
     public void typeAndHit(String item){
         typeAndEnter(searchBox,item);
-        LOG.info("type and hit  success");
+    
     }
 
     public void clickOnHomePageItem(){
         click(homepageItem);
-        LOG.info("click on home page item success");
+    
     }
 
 
     public void hoverOverHomePageItem(WebDriver driver){
         hoverOver(driver,homepageItem);
-        LOG.info("hover over home page item success");
+     
     }
 
     public void clickOnUserName(){
         click(userName);
-        LOG.info("click on user name success");
+
     }
 
 
     public void clickOnCartFromHomaPage(){
         click(CartFromHomePage);
-        LOG.info("click on cart from homepage success");
+     
     }
 
 
@@ -105,7 +104,7 @@ public class HomePage extends CommonAPI {
 
     public void clearSearchField(){
         clear(searchBox);
-        LOG.info("clear search field success");
+       
     }
 
 
