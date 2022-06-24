0ab221eb953fb94d8ebadb9e7f455c6975c04787
Foyez39 <105016210+Foyez39@users.noreply.github.com>
2022-06-24 11:34:40 -0500

    Update SignOutPage.java

--- src/main/java/pages/foyez/SignOutPage.java
+++ src/main/java/pages/foyez/SignOutPage.java
@@ -5,12 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 public class SignOutPage extends CommonAPI {
-    private final Logger LOG = LoggerFactory.getLogger(SignOutPage.class);
-    @FindBy(css = "a[title='Log me out']")
+ 
     private WebElement signOut;
 
     @FindBy(css = "a[title='Sign out']")
@@ -26,12 +24,12 @@ public class SignOutPage extends CommonAPI {
     //reusable steps
     public void clickOnSignOut(){
         click(signOut);
-        LOG.info("click on sign out success");
+        
     }
 
     public void clickOnSignOutFromFooter(){
         click(signOutFromFooter);
-        LOG.info("click on sign out from footer success");
+      
     }
 
 
