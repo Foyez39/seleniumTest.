ec0011be3bfbc3ed0491c280e1ddac0a4296aed5
Foyez39 <105016210+Foyez39@users.noreply.github.com>
2022-06-24 11:35:10 -0500

    Update WishlistPage.java

--- src/main/java/pages/foyez/WishlistPage.java
+++ src/main/java/pages/foyez/WishlistPage.java
@@ -5,12 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 public class WishlistPage extends CommonAPI {
 
-    private final Logger LOG = LoggerFactory.getLogger(WishlistPage.class);
+  
 
     @FindBy(css = "#product_condition")
     private WebElement productConditionAssertion;
@@ -42,13 +41,13 @@ public class WishlistPage extends CommonAPI {
 
     public void clickOnWishlist(){
         click(wishlist);
-        LOG.info("click on wishlist success");
+      
     }
 
 
     public void clickOnMyWishlist(){
         click(myWishlist);
-        LOG.info("click on my wishlist success");
+       
     }
 
 
